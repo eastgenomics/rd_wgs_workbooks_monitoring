b52caae68c566b1aa7e0537789568c0504812d68
kjwinfield <katherine.winfield1@nhs.net>
2024-06-28 17:04:23 +0100

    correct docstrings

--- rd_wgs_workbooks_monitoring.py
+++ rd_wgs_workbooks_monitoring.py
@@ -1,22 +1,21 @@
 """
 Script to monitor state of eggd_generate_rd_wgs_workbook jobs and flag those
-that have had workbooks made
+that have had workbooks made.
+Will verify that an .xlsx file has been made for a given file ID for a GEL JSON
 """
-import ast
 import dxpy as dx
-import os
 import argparse
 import json
 
-def parse_args() -> argparse.Namespace:
-    """
+def parse_args():
+    '''
     Parse command line arguments
 
     Returns
     -------
     args : Namespace
         Namespace of passed command line argument inputs
-    """
+    '''
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--json", "-i", required=True, help="DNAnexus file ID of GEL JSON"
@@ -30,7 +29,10 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument(
         "--rnumber", required=True, help="GEL R number for family"
     )
-    # TODO change below args to require=False once SHIRE integration is done
+    parser.add_argument(
+        "--time", required=True, help="Time since today to check."
+    )
+    # TODO change below args to required=True once SHIRE integration is done
     parser.add_argument("--uid", required=False, help="uid to connect server")
     parser.add_argument(
         "--password", "-pw", required=False, help="password to connect server"
@@ -39,12 +41,10 @@ def parse_args() -> argparse.Namespace:
 
 def dx_login(token):
     '''
-    Function to check authenticating to DNAneuxs
+    Function to check authenticating to DNAnexus
 
-    Parameters
-    ----------
-    token : str
-        DNAnexus authentication token
+    Inputs:
+        token (str): DNAnexus authentication token
     '''
     try:
         DX_SECURITY_CONTEXT = {
@@ -57,20 +57,23 @@ def dx_login(token):
     except dx.exceptions.InvalidAuthentication as err:
         print(err.error_message())
 
-def find_jobs(dx_project):
-    """
+def find_jobs(dx_project, time):
+    '''
     Find eggd_generate_rd_wgs_workbook jobs jobs that have run in the given
-    project in the last 48 hours
+    project in the given time period
 
-    Returns
-    -------
-    jobs : list
-        list of describe objects for each job
-    """
+    Inputs:
+        dx_project (str): DNAnexus project ID to search within
+        time (str): Time since today to check. Should be in format valid for
+        DX. See: http://autodoc.dnanexus.com/bindings/python/current/dxpy_sear
+        ch.html#dxpy.bindings.search.find_data_objects for details
+    Outputs:
+        jobs (list): list of describe objects for each job
+    '''
     jobs = list(dx.bindings.search.find_executions(
         project=dx_project,
         state='done',
-        created_after='-12h',
+        created_after=time,
         describe=True
     ))
 
@@ -90,8 +93,16 @@ def find_jobs(dx_project):
 
 def check_if_workbook_made(file_id, jobs):
     '''
-    Check if a workbook has been made with the query GEL RD WGS JSON as an
-    input
+    Check if a workbook has been made with the query GEL RD WGS JSON.
+    Do this by looking through a list of jobs and finding jobs that had the
+    JSON file ID as an input
+
+    Inputs:
+        file_id (str): DNAnexus file ID for query GEL RD WGS JSON
+        jobs (list): list of describe objects for generate_rd_wgs_workbook jobs
+    Outputs:
+        jobs (list): list of describe objects for generate_rd_wgs_workbook jobs
+        which had the query JSON file ID as an input
     '''
     matching_job = [
         x for x in jobs if
@@ -110,13 +121,22 @@ def get_output_xlsx_file_id(job):
     '''
     Get the DNAnexus file ID of the output xlsx report from a given
     eggd_generate_rd_wgs_workbook job
+
+    Inputs:
+        job (dict): generate_rd_wgs_workbook job describe dict
+    Outputs:
+        output_xlsx_id (str): File ID for output .xlsx report of the job 
     '''
     output_xlsx_id = job['describe']['output']['xlsx_report']['$dnanexus_link']
     return output_xlsx_id
 
 def check_if_correct_json_downloaded(json_file_id, rnumber):
     '''
-    Check that dx file is correct.
+    Check that a given JSON file ID is for the given GEL family ID (R number)
+
+    Inputs:
+        json_file_id (str): DNAnexus file ID for a GEL RD WGS JSON
+        rnumber (str): GEL family ID
     '''
     with dx.open_dxfile(json_file_id, mode='r') as f:
         c = f.read()
@@ -141,7 +161,7 @@ def monitor():
     args = parse_args()
 
     dx_login(args.dx_token)
-    jobs = find_jobs(args.dx_project)
+    jobs = find_jobs(args.dx_project, args.time)
 
     job_using_json = check_if_workbook_made(args.json, jobs)
     if len(job_using_json) == 1:
